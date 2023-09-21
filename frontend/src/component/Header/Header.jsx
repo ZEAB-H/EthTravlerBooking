ae22ef4344fa20ec4051b873e2fb4ac88c83d2e3
ZEAB-H <abelhailemichael30@gmail.com>
2023-09-21 21:57:26 +0300

    started developing the header part here

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import {Container, Row ,Button} from 'reactstrap';
+import { NavLink, Link } from 'react-router-dom';
+
+import Logo from '../../assets/images/logo.jpg'
+
+const nav_links=[
+
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'#',
+    display:'About'
+  },
+  {
+    path:'/tours',
+    display:'Tours'
+  },
+]
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <header className="header">
+    <Container>
+      <Row>
+        <div className="nav_wrapper d-flex align-items-center justify-content-between">
+
+          {/* ======== Logo Goes Here ========== */}
+            <div className="logo">
+              <img src={Logo} alt="" />
+            </div>
+            
+          {/* ======== Logo Ends Here ========== */}        </div>
+
+      </Row>
+    </Container>
+  </header>
+
 }
 
 export default Header
