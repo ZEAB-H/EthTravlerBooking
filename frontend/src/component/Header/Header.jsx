d6d45106de4dc8fd3f2e37396a336bdf4ce73516
ZEAB-H <abelhailemichael30@gmail.com>
2023-09-22 05:55:46 +0300

    listed the header part for the Web Header view

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -26,13 +26,46 @@ const Header = () => {
     <Container>
       <Row>
         <div className="nav_wrapper d-flex align-items-center justify-content-between">
-
           {/* ======== Logo Goes Here ========== */}
             <div className="logo">
               <img src={Logo} alt="" />
             </div>
             
-          {/* ======== Logo Ends Here ========== */}        </div>
+          {/* ======== Logo Ends Here ========== */}       
+          
+          {/* ======Menu Starts Here ==========*/}
+          
+          <div className="navigation">
+            <ul className="menu d-flex align-items-center gap-5">
+              {
+                nav_links.map((item,index)=>(
+
+                  <li className="nav__item" key={index}>
+                    <NavLink to={item.path}>{item.display}</NavLink>                    
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+
+          {/* ======Menu Starts Here ==========*/}
+
+          <div className="nav__right d-flex align-items-center gap-4 ">
+            <div className="nav__btns d-flex align-items-center gap-4">
+
+            <Button className="btn secondary__btn">
+                <Link to='/login'>Login</Link>
+              </Button>
+              <Button className="btn primary__btn">
+                <Link to='/register'>Register</Link>
+              </Button>
+            </div>
+
+            <span className="mobile__menu">
+              
+            </span>
+          </div>
+           </div>
 
       </Row>
     </Container>
