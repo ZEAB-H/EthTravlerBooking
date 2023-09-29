70520b2b5b99eb6f3a963d3f737570d28fa01504
ZEAB-H <abelhailemichael30@gmail.com>
2023-09-29 21:48:28 +0300

    added some thing

--- frontend/src/shared/Subtitle.jsx
+++ frontend/src/shared/Subtitle.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Subtitle = ()=>{
-    return(
-        <div>This is subtitle</div>
-    )
+const Subtitle = () => {
+  return (
+    <div>This is subtitle</div>
+  )
 }
 
 
 export default Subtitle;
+//here we will add the subtitle and everything
