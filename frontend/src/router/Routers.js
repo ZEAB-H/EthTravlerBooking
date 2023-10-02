97d96e3f90c88fea663805e87a2cd7bf51ee6787
ZEAB-H <abelhailemichael30@gmail.com>
2023-10-02 18:23:46 +0300

    changed the comment for the Routers page

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './../pages/Home'
 import Tours from './../pages/Tours'
 import TourDetails from './../pages/TourDetails'
@@ -7,18 +7,18 @@ import Login from './../pages/Login'
 import Register from './../pages/Register'
 import SearchResult from '../pages/SearchResult'
 
+//configuring the routers and navigation here
 
- 
 const Routers = () => {
   return (
     <Routes>
-      <Route path='/'  element={<Navigate to='/home'/>}/>
-      <Route path='/home'  element={<Home/>}/>
-      <Route path='/tours'  element={<Tours/>}/>
-      <Route path='/tours/:id'  element={<TourDetails/>}/>
-      <Route path='/login'  element={<Login/>}/>
-      <Route path='/register'  element={<Register/>}/>
-      <Route path='/tours/search'  element={<SearchResult/>}/>
+      <Route path='/' element={<Navigate to='/home' />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/tours' element={<Tours />} />
+      <Route path='/tours/:id' element={<TourDetails />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/tours/search' element={<SearchResult />} />
 
 
     </Routes>
